96ed9a514c7741c726ecbe33abea188e8728d670
dillontl <56282618+dillontl@users.noreply.github.com>
2020-05-03 20:56:50 -0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit();
 }
-$DATABASE_HOST = '107.180.58.91';
-$DATABASE_USER = 'dluera';
-$DATABASE_PASS = '1qaz@WSX3edc$RFV';
-$DATABASE_NAME = 'php_env';
+$DATABASE_HOST = 'ip or localhost';
+$DATABASE_USER = 'SQL Username';
+$DATABASE_PASS = 'SQL Password';
+$DATABASE_NAME = 'SQL DB Name';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
